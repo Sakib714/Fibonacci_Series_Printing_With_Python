49b2c434ce1aead899f0a0710b9d97f99a28d382
Sakib714 <sakib39.official@gmail.com>
2021-08-13 23:28:17 +0600

    Updated!

--- Printing Fibonnaci Series.py
+++ Printing Fibonnaci Series.py
@@ -2,17 +2,20 @@
 # 0, 1, 1, 2, 3, 5, 8, 13
 
 def fib(n):
-    a = 0
-    b = 1
-    if n==1:
-        print(a)
+    if n<1:
+        print('Please, Enter 1 or Greater NUmber!')
     else:
-        print(a)
-        print(b)
-        for i in range(2, n):
-            c = a+b
-            a = b
-            b = c
-            print(c)
+        a = 0
+        b = 1
+        if n==1:
+            print(a)
+        else:
+            print(a)
+            print(b)
+            for i in range(2, n):
+                c = a+b
+                a = b
+                b = c
+                print(c)
 
 fib(10)
